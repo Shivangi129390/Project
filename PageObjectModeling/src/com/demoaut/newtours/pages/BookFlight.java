58447f501b4365fcd28a5769fc74593e22b18581
jinav <jinav@jinavshivu.hitronhub.home>
2020-06-03 13:28:19 -0500

    Some changes

--- PageObjectModeling/src/com/demoaut/newtours/pages/BookFlight.java
+++ PageObjectModeling/src/com/demoaut/newtours/pages/BookFlight.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+// Add comment
 public class BookFlight {
 	@FindBy(name="passFirst0")
 	private WebElement f_name1;
